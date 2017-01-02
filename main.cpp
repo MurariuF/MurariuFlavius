dd5232152306de91b4603a63d20bdf0a9aab37a1
MurariuF <murariu.flavius@gmail.com>
2017-01-02 23:52:22 +0200

    al 4-lea

--- main.cpp
+++ main.cpp
@@ -183,9 +183,9 @@ void ARIE(int x, int y)
         cout<<"Ari(a):"<<x<<endl;
         cout<<"Acri(ac):"<<x/40.4685611<<endl;
         cout<<"Centimetri(cm):"<<x*1000000<<endl;
-        cout<<"Picioare(ft):"<<x/1076.426264<<endl;
-        cout<<"Hectari(ha):"<<<x/100<endl;
-        cout<<"Tol(in):"<<<x*154990.700557<endl;
+        cout<<"Picioare(ft):"<<x/1076.426<<endl;
+        cout<<"Hectari(ha):"<<x/100<<endl;
+        cout<<"Tol(in):"<<x*154990.700557<<endl;
         cout<<"Kilometri(km):"<<x/10000<<endl;
         cout<<"Metri(m):"<<x*100<<endl;
         cout<<"Milimetri(mm):"<<x*100000000<<endl;
@@ -197,8 +197,8 @@ void ARIE(int x, int y)
         cout<<"Acri(ac):"<<x<<endl;
         cout<<"Centimetri(cm):"<<x*40468561.19<<endl;
         cout<<"Picioare(ft):"<<x*43561.42216<<endl;
-        cout<<"Hectari(ha):"<<<x/2.471053<endl;
-        cout<<"Tol(in):"<<<x*6272250.64941<endl;
+        cout<<"Hectari(ha):"<<x/2.471053<<endl;
+        cout<<"Tol(in):"<<x*6272250.64941<<endl;
         cout<<"Kilometri(km):"<<x/247.1053999<<endl;
         cout<<"Metri(m):"<<x/4046.856119<<endl;
         cout<<"Milimetri(mm):"<<x*4046856119<<endl;
@@ -210,8 +210,8 @@ void ARIE(int x, int y)
         cout<<"Acri(ac):"<<x*2.471053<<endl;
         cout<<"Centimetri(cm):"<<x<<endl;
         cout<<"Picioare(ft):"<<x/929<<endl;
-        cout<<"Hectari(ha):"<<<x/6.452<endl;
-        cout<<"Tol(in):"<<<x<endl;
+        cout<<"Hectari(ha):"<<x/6.452<<endl;
+        cout<<"Tol(in):"<<x<<endl;
         cout<<"Kilometri(km):"<<x<<endl;
         cout<<"Metri(m):"<<x/10000<<endl;
         cout<<"Milimetri(mm):"<<x*100<<endl;
@@ -223,8 +223,8 @@ void ARIE(int x, int y)
         cout<<"Acri(ac):"<<x/43561.422163<<endl;
         cout<<"Centimetri(cm):"<<x*928.999999<<endl;
         cout<<"Picioare(ft):"<<x<<endl;
-        cout<<"Hectari(ha):"<<<x*107642.626481<endl;
-        cout<<"Tol(in):"<<<x*143.9863<endl;
+        cout<<"Hectari(ha):"<<x*107642.626481<<endl;
+        cout<<"Tol(in):"<<x*143.9863<<endl;
         cout<<"Kilometri(km):"<<x*9.28999999<<endl;
         cout<<"Metri(m):"<<x/10.764262<<endl;
         cout<<"Milimetri(mm):"<<x*92900<<endl;
@@ -236,8 +236,8 @@ void ARIE(int x, int y)
         cout<<"Acri(ac):"<<x*2.471053<<endl;
         cout<<"Centimetri(cm):"<<x*100000000<<endl;
         cout<<"Picioare(ft):"<<x*107642.62648<<endl;
-        cout<<"Hectari(ha):"<<<x<endl;
-        cout<<"Tol(in):"<<<x*15499070.0557<endl;
+        cout<<"Hectari(ha):"<<x<<endl;
+        cout<<"Tol(in):"<<x*15499070.0557<<endl;
         cout<<"Kilometri(km):"<<x/100<<endl;
         cout<<"Metri(m):"<<x*10000<<endl;
         cout<<"Milimetri(mm):"<<x*10000000000<<endl;
@@ -250,8 +250,8 @@ void ARIE(int x, int y)
         cout<<"Acri(ac):"<<x*1.594324<<endl;
         cout<<"Centimetri(cm):"<<x*6.4519999<<endl;
         cout<<"Picioare(ft):"<<x*143.98636<<endl;
-        cout<<"Hectari(ha):"<<<x<endl;
-        cout<<"Tol(in):"<<<x<endl;
+        cout<<"Hectari(ha):"<<x<<endl;
+        cout<<"Tol(in):"<<x<<endl;
         cout<<"Kilometri(km):"<<x<<endl;
         cout<<"Metri(m):"<<x/1549.907005<<endl;
         cout<<"Milimetri(mm):"<<x*645.2<<endl;
@@ -263,8 +263,8 @@ void ARIE(int x, int y)
         cout<<"Acri(ac):"<<x/247.1053999<<endl;
         cout<<"Centimetri(cm):"<<x*10000000000<<endl;
         cout<<"Picioare(ft):"<<x*10764262.648<<endl;
-        cout<<"Hectari(ha):"<<<x*100<endl;
-        cout<<"Tol(in):"<<<x*1549907005.5796<endl;
+        cout<<"Hectari(ha):"<<x*100<<endl;
+        cout<<"Tol(in):"<<x*1549907005.5796<<endl;
         cout<<"Kilometri(km):"<<x<<endl;
         cout<<"Metri(m):"<<x*1000000<<endl;
         cout<<"Milimetri(mm):"<<x*1000000000000<<endl;
@@ -277,8 +277,8 @@ void ARIE(int x, int y)
         cout<<"Acri(ac):"<<x/4046.856119<<endl;
         cout<<"Centimetri(cm):"<<x*10000<<endl;
         cout<<"Picioare(ft):"<<x*10.7642626<<endl;
-        cout<<"Hectari(ha):"<<<x/10000<endl;
-        cout<<"Tol(in):"<<<x*1549.907005<endl;
+        cout<<"Hectari(ha):"<<x/10000<<endl;
+        cout<<"Tol(in):"<<x*1549.907005<<endl;
         cout<<"Kilometri(km):"<<x/1000000<<endl;
         cout<<"Metri(m):"<<x<<endl;
         cout<<"Milimetri(mm):"<<x*1000000<<endl;
@@ -291,8 +291,8 @@ void ARIE(int x, int y)
         cout<<"Acri(ac):"<<x*2.471053<<endl;
         cout<<"Centimetri(cm):"<<x/100.000000000001<<endl;
         cout<<"Picioare(ft):"<<x/92900.000000074<<endl;
-        cout<<"Hectari(ha):"<<<x<endl;
-        cout<<"Tol(in):"<<<x*645.2<endl;
+        cout<<"Hectari(ha):"<<x<<endl;
+        cout<<"Tol(in):"<<x*645.2<<endl;
         cout<<"Kilometri(km):"<<x<<endl;
         cout<<"Metri(m):"<<x/1000000<<endl;
         cout<<"Milimetri(mm):"<<x<<endl;
@@ -304,8 +304,8 @@ void ARIE(int x, int y)
         cout<<"Acri(ac):"<<x/4839.99699<<endl;
         cout<<"Centimetri(cm):"<<x*8361.27816<<endl;
         cout<<"Picioare(ft):"<<x*9.0002994<<endl;
-        cout<<"Hectari(ha):"<<<x/11959.89394<endl;
-        cout<<"Tol(in):"<<<x*1295.92035957<endl;
+        cout<<"Hectari(ha):"<<x/11959.89394<<endl;
+        cout<<"Tol(in):"<<x*1295.92035957<<endl;
         cout<<"Kilometri(km):"<<x<<endl;
         cout<<"Metri(m):"<<x/1.195989<<endl;
         cout<<"Milimetri(mm):"<<x*836127.816<<endl;
@@ -331,7 +331,7 @@ void marimiVOLUM()
 void VOLUM(int x, int y)
 {
 
-    marimiVOLUM()'
+    marimiVOLUM();
     cout<<"Introduceti numarul pentru unitatea de masura pe care doriti sa o convertiti";
     cin>>y;
     if(y<0 && y>11) return;
@@ -367,7 +367,7 @@ void VOLUM(int x, int y)
     else if(y==3)
     {
         cout<<"Centilitri(cl):"<<x*10<<endl;
-        cout<<"Centrimetri(cm)":<<x*100<<endl;
+        cout<<"Centrimetri(cm):"<<x*100<<endl;
         cout<<"Decilitri(dl):"<<x<<endl;
         cout<<"Decimetri(dm):"<<x/10<<endl;
         cout<<"Picioare(ft):"<<x/283.20529<<endl;
@@ -381,7 +381,7 @@ void VOLUM(int x, int y)
     else if(y==4)
     {
         cout<<"Centilitri(cl):"<<x*100<<endl;
-        cout<<"Centrimetri(cm)":<<x*1000<<endl;
+        cout<<"Centrimetri(cm):"<<x*1000<<endl;
         cout<<"Decilitri(dl):"<<x*10<<endl;
         cout<<"Decimetri(dm):"<<x<<endl;
         cout<<"Picioare(ft):"<<x/18.688<<endl;
@@ -395,7 +395,7 @@ void VOLUM(int x, int y)
     else if(y==5)
     {
         cout<<"Centilitri(cl):"<<x*2832<<endl;
-        cout<<"Centrimetri(cm)":<<x*28320<<endl;
+        cout<<"Centrimetri(cm):"<<x*28320<<endl;
         cout<<"Decilitri(dl):"<<x*283.2<<endl;
         cout<<"Decimetri(dm):"<<x*28.32<<endl;
         cout<<"Picioare(ft):"<<x<<endl;
@@ -409,7 +409,7 @@ void VOLUM(int x, int y)
     else if(y==6)
     {
         cout<<"Centilitri(cl):"<<x*454.599<<endl;
-        cout<<"Centrimetri(cm)":<<x*4546<<endl;
+        cout<<"Centrimetri(cm):"<<x*4546<<endl;
         cout<<"Decilitri(dl):"<<x*45.4599<<endl;
         cout<<"Decimetri(dm):"<<x*4.54599<<endl;
         cout<<"Picioare(ft):"<<x/6.2305<<endl;
@@ -423,7 +423,7 @@ void VOLUM(int x, int y)
     else if(y==7)
     {
         cout<<"Centilitri(cl):"<<x*1.6387<<endl;
-        cout<<"Centrimetri(cm)":<<x*16.387<<endl;
+        cout<<"Centrimetri(cm):"<<x*16.387<<endl;
         cout<<"Decilitri(dl):"<<x/6.105<<endl;
         cout<<"Decimetri(dm):"<<x/61.05<<endl;
         cout<<"Picioare(ft):"<<x/1728.3097<<endl;
@@ -437,7 +437,7 @@ void VOLUM(int x, int y)
     else if(y==8)
     {
         cout<<"Centilitri(cl):"<<x*100<<endl;
-        cout<<"Centrimetri(cm)":<<x*1000<<endl;
+        cout<<"Centrimetri(cm):"<<x*1000<<endl;
         cout<<"Decilitri(dl):"<<x*10<<endl;
         cout<<"Decimetri(dm):"<<x<<endl;
         cout<<"Picioare(ft):"<<x/28.3205<<endl;
@@ -451,10 +451,10 @@ void VOLUM(int x, int y)
     else if(y==9)
     {
         cout<<"Centilitri(cl):"<<x/10<<endl;
-        cout<<"Centrimetri(cm)":<<x<<endl;
+        cout<<"Centrimetri(cm):"<<x<<endl;
         cout<<"Decilitri(dl):"<<x/100<<endl;
         cout<<"Decimetri(dm):"<<x/1000<<endl;
-        cout<<"Picioare(ft):"<<x</2832.0589<endl;
+        cout<<"Picioare(ft):"<<x/2832.0589<<endl;
         cout<<"Galon(gal):"<<x/4547.5216<<endl;
         cout<<"Tol(in):"<<x/16.3872<<endl;
         cout<<"Litri(l):"<<x/1000<<endl;
@@ -465,7 +465,7 @@ void VOLUM(int x, int y)
     else if(y==10)
     {
         cout<<"Centilitri(cl):"<<x*1000000<<endl;
-        cout<<"Centrimetri(cm)":<<x*1000000<<endl;
+        cout<<"Centrimetri(cm):"<<x*1000000<<endl;
         cout<<"Decilitri(dl):"<<x*10000<<endl;
         cout<<"Decimetri(dm):"<<x*1000<<endl;
         cout<<"Picioare(ft):"<<x*25.3107<<endl;
@@ -480,7 +480,7 @@ void VOLUM(int x, int y)
     {
 
         cout<<"Centilitri(cl):"<<x*76456<<endl;
-        cout<<"Centrimetri(cm)":<<x*764560<<endl;
+        cout<<"Centrimetri(cm):"<<x*764560<<endl;
         cout<<"Decilitri(dl):"<<x/7645.6<<endl;
         cout<<"Decimetri(dm):"<<x/764.56<<endl;
         cout<<"Picioare(ft):"<<x*26.9971<<endl;
@@ -541,6 +541,118 @@ void TIMP(int x, int y)
         cout<<"Nanosecunda(ns): "<<x*10000000<<endl;
         cout<<"Picosecunda(ps): "<<x*10000000000<<endl;
         cout<<"Secunda(s): "<<x/100<<endl;
+    }
+    else if(y==3)
+    {
+        cout<<"Attosecunda(as): "<<x*100000000000000000<<endl;
+        cout<<"Centisecunda(cs): "<<x*10<<endl;
+        cout<<"Decisecunda(ds): "<<x<<endl;
+        cout<<"Femtosecunda(fs): "<<x*100000000000000<<endl;
+        cout<<"Ora(h): "<<x/36000<<endl;
+        cout<<"Megaannum(ma): "<<x*3.1732<<endl;
+        cout<<"Minute(min): "<<x/600<<endl;
+        cout<<"Milisecunda(ms): "<<x*100<<endl;
+        cout<<"Nanosecunda(ns): "<<x*100000000<<endl;
+        cout<<"Picosecunda(ps): "<<x*100000000000<<endl;
+        cout<<"Secunda(s): "<<x/10<<endl;
+    }
+    else if(y==4)
+    {
+        cout<<"Attosecunda(as): "<<x*1000<<endl;
+        cout<<"Centisecunda(cs): "<<x/1e-13<<endl;
+        cout<<"Decisecunda(ds): "<<x*1e-14<<endl;
+        cout<<"Femtosecunda(fs): "<<x<<endl;
+        cout<<"Ora(h): "<<x*2.777777777777778e-19<<endl;
+        cout<<"Megaannum(ma): "<<x*3.1732331437855404e-30<<endl;
+        cout<<"Minute(min): "<<x*1.6666666666666667e-17<<endl;
+        cout<<"Milisecunda(ms): "<<x*1e-12<<endl;
+        cout<<"Nanosecunda(ns): "<<x/1000000<<endl;
+        cout<<"Picosecunda(ps): "<<x/1000<<endl;
+        cout<<"Secunda(s): "<<x*1e-15<<endl;
+    }
+    else if(y==5)
+    {
+        cout<<"Attosecunda(as): "<<x*3.6e+21<<endl;
+        cout<<"Centisecunda(cs): "<<x*360000<<endl;
+        cout<<"Decisecunda(ds): "<<x*36000<<endl;
+        cout<<"Femtosecunda(fs): "<<x*3599999999999999500<<endl;
+        cout<<"Ora(h): "<<x<<endl;
+        cout<<"Megaannum(ma): "<<x*1.1423639317627944e-11<<endl;
+        cout<<"Minute(min): "<<x*60<<endl;
+        cout<<"Milisecunda(ms): "<<x*3600000<<endl;
+        cout<<"Nanosecunda(ns): "<<x*3600000000000<<endl;
+        cout<<"Picosecunda(ps): "<<x*3600000000000000<<endl;
+        cout<<"Secunda(s): "<<x*3600<<endl;
+    }
+    else if(y==6)
+    {
+        cout<<"Attosecunda(as): "<<x*3.15136e+32<<endl;
+        cout<<"Centisecunda(cs): "<<x*31513600000000000<<endl;
+        cout<<"Decisecunda(ds): "<<x*3151360000000000<<endl;
+        cout<<"Femtosecunda(fs): "<<x*3.15136e+29<<endl;
+        cout<<"Ora(h): "<<x*87537777777.77777<<endl;
+        cout<<"Megaannum(ma): "<<x<<endl;
+        cout<<"Minute(min): "<<x*5252266666666.667<<endl;
+        cout<<"Milisecunda(ms): "<<x*315136000000000000<<endl;
+        cout<<"Nanosecunda(ns): "<<x*3.15136e+23<<endl;
+        cout<<"Picosecunda(ps): "<<x*3.15136e+26<<endl;
+        cout<<"Secunda(s): "<<x*315136000000000<<endl;
+    }
+    else if(y==7)
+    {
+        cout<<"Attosecunda(as): "<<x*59999999999999990000<<endl;
+        cout<<"Centisecunda(cs): "<<x*6000<<endl;
+        cout<<"Decisecunda(ds): "<<x*600<<endl;
+        cout<<"Femtosecunda(fs): "<<x*59999999999999990<<endl;
+        cout<<"Ora(h): "<<x/60<<endl;
+        cout<<"Megaannum(ma): "<<x*1.903939886271324e-13<<endl;
+        cout<<"Minute(min): "<<x<<endl;
+        cout<<"Milisecunda(ms): "<<x*60000<<endl;
+        cout<<"Nanosecunda(ns): "<<x*60000000000<<endl;
+        cout<<"Picosecunda(ps): "<<x*60000000000000<<endl;
+        cout<<"Secunda(s): "<<x*60<<endl;
+    }
+    else if(y==8)
+    {
+        cout<<"Attosecunda(as): "<<x*1000000000000000<<endl;
+        cout<<"Centisecunda(cs): "<<x/10<<endl;
+        cout<<"Decisecunda(ds): "<<x/100<<endl;
+        cout<<"Femtosecunda(fs): "<<x*1000000000000<<endl;
+        cout<<"Ora(h): "<<x*2.7777777777777776e-7<<endl;
+        cout<<"Megaannum(ma): "<<x*3.1732331437855403e-18<<endl;
+        cout<<"Minute(min): "<<x*60000<<endl;
+        cout<<"Milisecunda(ms): "<<x<<endl;
+        cout<<"Nanosecunda(ns): "<<x*1000000<<endl;
+        cout<<"Picosecunda(ps): "<<x*1000000000<<endl;
+        cout<<"Secunda(s): "<<x/1000<<endl;
+    }
+    else if(y==9)
+    {
+        cout<<"Attosecunda(as): "<<x*1000000000<<endl;
+        cout<<"Centisecunda(cs): "<<x*1.0000000000000001e-7<<endl;
+        cout<<"Decisecunda(ds): "<<x*1e-8<<endl;
+        cout<<"Femtosecunda(fs): "<<x*1000000<<endl;
+        cout<<"Ora(h): "<<x*2.777777777777778e-13<<endl;
+        cout<<"Megaannum(ma): "<<x*3.1732331437855405e-24<<endl;
+        cout<<"Minute(min): "<<x*1.6666666666666667e-11<<endl;
+        cout<<"Milisecunda(ms): "<<x/1000000<<endl;
+        cout<<"Nanosecunda(ns): "<<x<<endl;
+        cout<<"Picosecunda(ps): "<<x*1000<<endl;
+        cout<<"Secunda(s): "<<x*1e-9<<endl;
+    }
+    else if(y==10)
+    {
+        cout<<"Attosecunda(as): "<<x<<endl;
+        cout<<"Centisecunda(cs): "<<x<<endl;
+        cout<<"Decisecunda(ds): "<<x<<endl;
+        cout<<"Femtosecunda(fs): "<<x<<endl;
+        cout<<"Ora(h): "<<x<<endl;
+        cout<<"Megaannum(ma): "<<x<<endl;
+        cout<<"Minute(min): "<<x<<endl;
+        cout<<"Milisecunda(ms): "<<x<<endl;
+        cout<<"Nanosecunda(ns): "<<x<<endl;
+        cout<<"Picosecunda(ps): "<<x<<endl;
+        cout<<"Secunda(s): "<<x<<endl;
     }
 
 }
